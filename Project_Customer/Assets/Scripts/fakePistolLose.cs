71642983cbe80e415034820011f3024318b6d238
ScrungeDistrugatorul <andigebg@gmail.com>
2022-09-20 20:33:15 +0200

    Add

--- Project_Customer/Assets/Scripts/fakePistolLose.cs
+++ Project_Customer/Assets/Scripts/fakePistolLose.cs
@@ -5,16 +5,24 @@ public class FakePistolLose : MonoBehaviour
 {
     public GameObject player;
     private PlayerMovement _playerMovement;
+    
+    public GameObject ammoBox;
+    private AmmoSwitch _ammoSwitch;
 
     private void Start()
     {
         _playerMovement = player.GetComponent<PlayerMovement>();
+        _ammoSwitch = ammoBox.GetComponent<AmmoSwitch>();
     }
     private void OnBecameInvisible()
     {
         if (gameObject.activeSelf && _playerMovement.gunLoaded)
         {
             SceneManager.LoadScene("Retry_Scene");
+        }
+        else if (gameObject.activeSelf && _playerMovement.gunLoaded == false && _ammoSwitch.hasAmmo == false)
+        {
+            SceneManager.LoadScene("UnloadedRetryScene");
         }
     }
 }
